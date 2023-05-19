a4e5de160f0757c289a03a0823e5a4e39976b69f
Amanda Asgari-Rad <amanda_asgarirad@yahoo.com>
2023-05-19 10:43:51 -0500

    commit all

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+Main();
+
+void Main()
+{
+
+    Console.WriteLine("Welcome to the Magic Moose Simulator!");
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine();
+
+    MooseSays("HI, I'M MAGIC!");
+    Console.Write($"Ask A Question: ");
+    string question = Console.ReadLine().ToLower();
+
+    if (question.Length > 0)
+    {
+        MooseAnswers();
+    }
+
+}
+
+void MooseSays(string message)
+{
+    Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |   {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
+
+
+// google how to store several strings.
+// how to configure choosing a random string when the method is invoked.
+
+// create method that initiates the user to ask a question.
+void MooseAnswers()
+{
+    string[] answers = { "As I see it, yes", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "It is certain", "It is decidedly so", "Most likely", "My reply is no", "My sources say no", "Outlook not so good", "Outlook good", "Reply hazy, try again", "Signs point to yes", "Very doubtful", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it" };
+    Random random = new Random();
+    int answer = random.Next(0, 19);
+
+    Console.WriteLine(answers[answer]);
+}
